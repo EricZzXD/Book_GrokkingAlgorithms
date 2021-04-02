77c6d519e4286ba459e9dac4f88a483b9660922d
EricZzXD <yuhuazhao52@gmail.com>
2021-04-02 17:54:34 +1100

    Chapter_4_Note_update

--- Chapter_4.py
+++ Chapter_4.py
@@ -19,7 +19,13 @@ def main():
     print "-----"
     BinarySearch_array = [2, 4, 6, 8, 10]
     BinarySearch_target = 8
-    print "exercise4_4: " + str(exercise4_3(BinarySearch_array, BinarySearch_target))
+    First_Index = 0
+    Last_Index = len(BinarySearch_array) - 1
+    print "exercise4_4: " + str(exercise4_4(BinarySearch_array, BinarySearch_target, First_Index, Last_Index))
+
+    print "-----"
+    quicksort_array = [2, 8, 7, 2, 4, 6, 9, 2, 1]
+    print "Quick sort: " + str(quicksort(quicksort_array))
 
 
 # 4.1 -> 分儿治之 (书:P45)
@@ -53,9 +59,31 @@ def exercise4_3(Max_array):
     return max(Max_array.pop(), exercise4_3(Max_array))
 
     # 练习 4.4  -> Use recursion to do binary search
-def exercise4_4(BinarySearch_array, BinarySearch_target):
+def exercise4_4(BinarySearch_array, BinarySearch_target, First_Index, Last_Index):
+    if First_Index < Last_Index:
+        Mid_index = (First_Index + Last_Index) / 2
+        if BinarySearch_array[Mid_index] == BinarySearch_target:
+            return Mid_index
+        elif First_Index == Last_Index:
+            return None
+        elif BinarySearch_array[Mid_index] < BinarySearch_target:
+            First_Index = Mid_index + 1
+        else:
+            Last_Index = Mid_index - 1
 
-    return
+    return exercise4_4(BinarySearch_array, BinarySearch_target, First_Index, Last_Index)
+
+
+# 4.2 -> 快排算法 (书:P52)
+def quicksort(quicksort_array):
+    if len(quicksort_array) < 2:
+        return quicksort_array
+    else:
+        pivot = quicksort_array[0]
+        # Cause the quicksort_array[0] selected as pivot (so exclude the pivot)
+        less = [i for i in quicksort_array[1:] if i <= pivot]
+        greater = [i for i in quicksort_array[1:] if i > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
 
 
 main()
